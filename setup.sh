06d20c1bdd25bb287f4db03e736bbb990a9b606d
Max Braun <max.braun@gmail.com>
2020-08-23 12:59:33 -0700

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 1. Enable camera and serial interfaces: sudo raspi-config
+# 2. Run this script: sudo ./setup.sh
+
+
+set -e
+
+cd /home/pi
+sudo apt-get install -y git
+git clone https://github.com/maxbbraun/uvc-gadget
+cd uvc-gadget
+
+make
+
+sudo cp piwebcam.service /etc/systemd/system/
+sudo systemctl enable piwebcam
+
+sudo sed -i 's/^console=\(.*\)$/\1 modules-load=dwc2,libcomposite/' /boot/cmdline.txt
+
+sudo echo "dtoverlay=dwc2" >> /boot/config.txt
+
+sudo ln -s /lib/systemd/system/getty@.service /etc/systemd/system/getty.target.wants/getty@ttyGS0.service
