8d1d19db49decbee5411f8883c5b11ec5ca668e4
gigawatts <1954516+gigawatts@users.noreply.github.com>
2020-08-28 20:33:27 -0500

    corrected writing to /boot/config.txt with sudo tee
    Reason: https://stackoverflow.com/questions/84882/sudo-echo-something-etc-privilegedfile-doesnt-work

--- setup.sh
+++ setup.sh
@@ -18,6 +18,6 @@ sudo systemctl enable piwebcam
 
 sudo sed -i 's/^console=\(.*\)$/\1 modules-load=dwc2,libcomposite/' /boot/cmdline.txt
 
-sudo printf "\ndtoverlay=dwc2" >> /boot/config.txt
+printf "\ndtoverlay=dwc2" | sudo tee -a /boot/config.txt
 
 sudo ln -s /lib/systemd/system/getty@.service /etc/systemd/system/getty.target.wants/getty@ttyGS0.service
