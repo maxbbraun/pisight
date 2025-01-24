83f3519f2a4a21c3569e0ec4c686ccd395a3f3de
vguttmann <61903989+vguttmann@users.noreply.github.com>
2025-01-24 18:27:37 +0100

    Update setup.sh
    the firmware files moved from /boot to /boot/firmware, changed them to reflect that

--- setup.sh
+++ setup.sh
@@ -15,8 +15,8 @@ make
 sudo cp piwebcam.service /etc/systemd/system/
 sudo systemctl enable piwebcam
 
-sudo sed -i 's/^console=\(.*\)$/\1 modules-load=dwc2,libcomposite/' /boot/cmdline.txt
+sudo sed -i 's/^console=\(.*\)$/\1 modules-load=dwc2,libcomposite/' /boot/firmware/cmdline.txt
 
-printf "\ndtoverlay=dwc2\n" | sudo tee -a /boot/config.txt
+printf "\ndtoverlay=dwc2\n" | sudo tee -a /boot/firmware/config.txt
 
 sudo ln -s /lib/systemd/system/getty@.service /etc/systemd/system/getty.target.wants/getty@ttyGS0.service
