d601628830ea7d4912a8c4c7426afdb8552e3202
Max Braun <max.braun@gmail.com>
2025-02-12 23:44:01 -0800

    Install and enable the service during setup

--- setup.sh
+++ setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 1. Enable camera and serial interfaces: sudo raspi-config
-# 2. Run this script: sudo ./setup.sh
-
 set -e
 
 echo "dtoverlay=dwc2,dr_mode=otg" | tee -a /boot/firmware/config.txt
@@ -12,12 +9,14 @@ sudo apt install -y git meson libcamera-dev libjpeg-dev
 git clone https://gitlab.freedesktop.org/camera/uvc-gadget.git
 
 cd /home/pi/uvc-gadget
-
 make uvc-gadget
 cd build
 meson install
 ldconfig
 
 chmod +x /home/pi/pisight/rpi-uvc-gadget.sh
-cp /home/pi/pisight/rpi-uvc-gadget.sh /usr/local/bin/rpi-uvc-gadget.sh
-cp /home/pi/pisight/uvc-gadget.service /etc/systemd/system/uvc-gadget.service
+sudo cp /home/pi/pisight/rpi-uvc-gadget.sh /usr/local/bin/rpi-uvc-gadget.sh
+sudo cp /home/pi/pisight/uvc-gadget.service /etc/systemd/system/uvc-gadget.service
+sudo systemctl daemon-reload
+sudo systemctl enable uvc-gadget.service
+sudo systemctl start uvc-gadget.service
